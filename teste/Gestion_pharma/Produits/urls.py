85ae6a72447c74fb5d3ec7d8afc6c13aa3c3bd09
RomakaBenoit <romakabenoit18@gmail.com>
2024-12-04 04:56:26 +0100

    nous avons gere les images des produits

--- teste/Gestion_pharma/Produits/urls.py
+++ teste/Gestion_pharma/Produits/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('',home,name='home'),
-]
+    #path('',home,name='home'),
+
+    path('',Affichage.as_view(), name='home'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- teste/Gestion_pharma/Produits/views.py
+++ teste/Gestion_pharma/Produits/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView
+
 from .models import *
 
-# Create your views here.
-def home(request):
+# # Create your views here.
+# def home(request):
 
-    #recuperation de toutes les valeurs quise trouve dans la bade donnees
-    donnees = Produits.objects.all
+#     #recuperation de toutes les valeurs quise trouve dans la bade donnees
+#     # donnees = Produits.objects.all
 
-    context ={
-        'donnees':donnees
-    }
+#     # context ={
+#     #     'donnees':donnees
+#     # }
 
-    return render(request,'home.html',context)
+#     return render(request,'home.html',context)
+
+
+#faisons une autre forme d'affichage fonde sur des class
+
+class Affichage(ListView):
+    #affichage du templete 
+    template_name= 'home.html'
+    #recuperation des info de la Bdd
+    queryset = Produits.objects.all()
